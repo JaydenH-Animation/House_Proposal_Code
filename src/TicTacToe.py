c65c2a8782c7f58291aea7c58f1bd1e81ba236c5
JaydenH-Animation <jayden.houseusmc@gmail.com>
2025-05-12 12:55:22 -0500

    Created Board

--- src/TicTacToe.py
+++ src/TicTacToe.py
@@ -1,8 +1,23 @@
-import random
+board = ["_", "_", "_",
+        "_", "_", "_",
+        "_", "_", "_"]
+currrentPlayer = "X"
+winner = None
+gameRunning= True
+
 
 #Make the Game Board
+def printBoard(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print("________")
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print("________")
+    print(board[6] + " | " + board[7] + " | " + board[8])
+    printBoard(board)
+    print("________")
 
 # take Player input
+
 
 # Check for Win or Tie
 
