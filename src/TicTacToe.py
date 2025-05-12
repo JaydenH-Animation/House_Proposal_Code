a0266d7c0005a724ba6418f96323c2f95079120e
JaydenH-Animation <jayden.houseusmc@gmail.com>
2025-05-12 13:11:04 -0500

    The tutorial I followed wouldn't print the board. In addition I wouldn't have been a pop up window. So I found a new tutotria.
    tldr: new turotrial with popup window

--- src/TicTacToe.py
+++ src/TicTacToe.py
@@ -1,26 +1,28 @@
-board = ["_", "_", "_",
-        "_", "_", "_",
-        "_", "_", "_"]
-currrentPlayer = "X"
-winner = None
-gameRunning= True
+import tkinter as tk
+from tkinter import messagebox
+import random
 
-
-#Make the Game Board
-def printBoard(board):
-    print(board[0] + " | " + board[1] + " | " + board[2])
-    print("________")
-    print(board[3] + " | " + board[4] + " | " + board[5])
-    print("________")
-    print(board[6] + " | " + board[7] + " | " + board[8])
-    printBoard(board)
-    print("________")
-
+# Make the Game Board
 # take Player input
-
-
 # Check for Win or Tie
-
 # Switch the player 
-
 # Check for win or tie again
+# Add Ai Elements
+
+def next_turn():
+    pass
+
+def check_winner():
+    pass
+
+def empty_spaces():
+    pass
+
+def new_game():
+    pass
+
+#Main Window setup
+window = tk.Tk()
+window.title("Tic Tac Toe")
+window.geometry("400x500")  # Window Size
+window.mainloop()
